55825aa0e85865148d62f78f79aeece6f62cc1aa
Josh Mann <jlmann72@gmail.com>
2020-09-03 14:47:31 -0400

    Created Pet class and Pets table

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,7 +14,7 @@ namespace TamagotchiAPI.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
+        public DbSet<pet> Pets { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
